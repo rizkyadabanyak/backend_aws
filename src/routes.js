067e44bfe82ce34fefb15a2a909ed7b83b657373
rizky <rizkypr@rnd.ts.co.id>
2023-05-23 00:24:09 +0700

    huh

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ const routes = [
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "Sukses";
+      return "Hello World!, oh yeaahhh woho... berhasil. lelah ya... mangat...";
     },
   },
   // create date (POST)
