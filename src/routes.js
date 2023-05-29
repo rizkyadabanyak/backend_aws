cff7a93405f015305b638edc973ab1098fa910cd
rizky <rizkypr@rnd.ts.co.id>
2023-05-30 01:57:19 +0700

    guft

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ const routes = [
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "tolong bisa dongggxxxxxx";
+      return "tolong bisa haha";
     },
   },
   // create date (POST)
