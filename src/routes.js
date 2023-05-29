b88918e2054f213c193b5d21fe2626e610f00ae2
rizky <rizkypr@rnd.ts.co.id>
2023-05-29 20:02:41 +0700

    re createsssss

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ const routes = [
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "ini ganti bang, hah....";
+      return "huft";
     },
   },
   // create date (POST)
