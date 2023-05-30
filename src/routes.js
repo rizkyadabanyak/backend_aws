b47ca169dfb630457ac036548043b0a5740e847d
rizky <rizkypr@rnd.ts.co.id>
2023-05-30 07:59:34 +0700

    wasap

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ const routes = [
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "tolong bisa xxx";
+      return "wasap men";
     },
   },
   // create date (POST)
